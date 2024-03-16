6b41c46a4d23645d41d948630a25d06831bd0bbc
Muarem <s.muarem10@gmail.com>
2024-03-16 14:02:46 +0400

    collect flow in View Model

--- app/src/main/java/com/imsosoft/kotlinflow/MyViewModel.kt
+++ app/src/main/java/com/imsosoft/kotlinflow/MyViewModel.kt
@@ -1,10 +1,15 @@
 package com.imsosoft.kotlinflow
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 class MyViewModel: ViewModel() {
+
+
+
 
     val countDownTimer = flow<Int> {
 
@@ -19,6 +24,61 @@ class MyViewModel: ViewModel() {
             emit(counter)
         }
 
+    }
+
+
+    init {
+        collectFlow()
+    }
+
+    private fun collectFlow() {
+
+        viewModelScope.launch {
+            countDownTimer
+                .filter {
+                    it % 2 == 0
+                }
+                .map {
+                    it * it
+                }
+                .collect {
+                println("Counter is $it")
+            }
+        }
+
+        // 1 and 2 are the same
+
+        /* 1.
+
+        countDownTimer.onEach {
+            delay(1000)
+            println(it)
+        }.launchIn(viewModelScope)
+
+
+
+         */
+
+        /* 2.
+        viewModelScope.launch {
+            countDownTimer.collect {
+                println(it)
+            }
+        }
+
+         */
+
+
+        /* 3.
+        viewModelScope.launch {
+            countDownTimer.collectLatest {
+                delay(2000)
+                println(it)
+            }
+        }
+
+
+         */
     }
 
 }
